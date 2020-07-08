07c4e3d81185e96af23d9cb38fab4912b8ee316b
Christopher Hunter <hunter.christopher@icloud.com>
2020-07-07 23:56:43 -0700

    implement a parser for quote responses

--- client.go
+++ client.go
@@ -1,8 +1,12 @@
 package alphavantage
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 )
 
 type Doer interface {
@@ -24,10 +28,61 @@ func (service Service) Do(req *http.Request) (*http.Response, error) {
 	return service.Client.Do(req)
 }
 
-// func (service Service) QueryStocks(symbol string) ([]StockQuotes, error) {
-// 	res, _ := service.Do(fmt.Sprintf("/query?symbol=%s", symbol))
-// 	buf, _ := ioutil.ReadAll(res)
-// 	var quotes []StockQuote
-// 	json.Unmarshal(buf, &quotes)
-// 	return quotes, nil
-// }
+type Quote struct {
+	Time                           time.Time
+	Open, High, Low, Close, Volume float64
+}
+
+func ParseQueryResponse(r io.Reader) ([]Quote, error) {
+	body := make(map[string]interface{})
+
+	if err := json.NewDecoder(r).Decode(&body); err != nil {
+		return nil, err
+	}
+
+	delete(body, "Meta Data")
+
+	var values interface{}
+	for _, values = range body {
+		break
+	}
+
+	rawQuotes := values.(map[string]interface{})
+
+	var quotes []Quote
+
+	for rawTime, values := range rawQuotes {
+		tm, err := time.Parse("2006-01-02 15:04:05", rawTime)
+		if err != nil {
+			continue
+		}
+
+		valuesMap := values.(map[string]interface{})
+
+		var quote Quote
+
+		quote.Time = tm
+
+		for k, val := range valuesMap {
+			f := strings.Fields(k)
+			if len(f) < 2 {
+				continue
+			}
+			switch f[1] {
+			case "open":
+				quote.Open = val.(float64)
+			case "high":
+				quote.High = val.(float64)
+			case "low":
+				quote.Low = val.(float64)
+			case "close":
+				quote.Close = val.(float64)
+			case "volume":
+				quote.Volume = val.(float64)
+			}
+		}
+		quotes = append(quotes, quote)
+	}
+
+	return quotes, nil
+}
