8b4798b6ec08c3e50585ee4c261f289e88fd462e
Christopher Hunter <hunter.christopher@icloud.com>
2020-07-12 18:02:17 -0700

    implement query response parser

--- client.go
+++ client.go
@@ -2,11 +2,26 @@ package alphavantage
 
 import (
 	"encoding/json"
-	"io"
+	"fmt"
 	"net/http"
 	"net/url"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
+)
+
+const (
+	metaDataKey = "Meta Data"
+
+	timeSeriesDaily          = "Time Series (Daily)"
+	timeSeriesWeekly         = "Weekly Time Series"
+	timeSeriesWeeklyAdjusted = "Weekly Adjusted Time Series"
+	timeSeriesIntraday1min   = "Time Series (1min)"
+	timeSeriesIntraday5min   = "Time Series (5min)"
+	timeSeriesIntraday15min  = "Time Series (15min)"
+	timeSeriesIntraday30min  = "Time Series (30min)"
+	timeSeriesIntraday60min  = "Time Series (60min)"
 )
 
 type Doer interface {
@@ -23,8 +38,16 @@ func (service Service) Do(req *http.Request) (*http.Response, error) {
 	if service.Client == nil {
 		service.Client = http.DefaultClient
 	}
-	req.URL, _ = url.Parse("https://www.alphavantage.co")
-	req.URL.Query().Set("apikey", service.APIKey)
+
+	u, _ := url.Parse("https://www.alphavantage.co")
+	req.URL.Host = u.Host
+	req.URL.Scheme = u.Scheme
+
+	q := req.URL.Query()
+	if q.Get("apikey") == "" {
+		q.Set("apikey", service.APIKey)
+	}
+	req.URL.RawQuery = q.Encode()
 	return service.Client.Do(req)
 }
 
@@ -33,56 +56,141 @@ type Quote struct {
 	Open, High, Low, Close, Volume float64
 }
 
-func ParseQueryResponse(r io.Reader) ([]Quote, error) {
-	body := make(map[string]interface{})
+func ParseQuotesResponse(in []byte) ([]Quote, error) {
+	body := make(map[string]json.RawMessage)
 
-	if err := json.NewDecoder(r).Decode(&body); err != nil {
+	if err := json.Unmarshal(in, &body); err != nil {
 		return nil, err
 	}
 
-	delete(body, "Meta Data")
+	var rawMeta map[string]string
 
-	var values interface{}
-	for _, values = range body {
+	if err := json.Unmarshal(body[metaDataKey], &rawMeta); err != nil {
+		return nil, fmt.Errorf("could not decode meta: %w", err)
+	}
+
+	var (
+		timeZone *time.Location
+	)
+
+	for k, val := range rawMeta {
+		switch {
+		// case strings.HasSuffix(k, "Last Refreshed"):
+
+		case strings.HasSuffix(k, "Time Zone"):
+			timeZone, _ = time.LoadLocation(val)
+		}
+	}
+
+	if timeZone == nil {
+		return nil, fmt.Errorf("could not parse timezone from meta")
+	}
+
+	delete(body, metaDataKey)
+
+	var (
+		rawQuotes map[string]json.RawMessage
+		format    = "2006-01-02"
+	)
+
+	for key := range body {
+		switch key {
+		default:
+			return nil, fmt.Errorf("unknown key %q", key)
+
+		case timeSeriesDaily,
+			timeSeriesWeekly,
+			timeSeriesWeeklyAdjusted:
+
+		case timeSeriesIntraday1min,
+			timeSeriesIntraday5min,
+			timeSeriesIntraday15min,
+			timeSeriesIntraday30min,
+			timeSeriesIntraday60min:
+			format += " 15:04:05"
+		}
+
+		if err := json.Unmarshal(body[key], &rawQuotes); err != nil {
+			return nil, fmt.Errorf("could not decode quote: %w", err)
+		}
+
 		break
 	}
 
-	rawQuotes := values.(map[string]interface{})
-
 	var quotes []Quote
 
-	for rawTime, values := range rawQuotes {
-		tm, err := time.Parse("2006-01-02 15:04:05", rawTime)
+	for rawTime, rawQuote := range rawQuotes {
+		tm, err := time.ParseInLocation(format, rawTime, timeZone)
 		if err != nil {
 			continue
 		}
 
-		valuesMap := values.(map[string]interface{})
+		quote := Quote{
+			Time: tm,
+		}
 
-		var quote Quote
+		var valuesMap map[string]string
 
-		quote.Time = tm
+		if err := json.Unmarshal(rawQuote, &valuesMap); err != nil {
+			return nil, err
+		}
 
 		for k, val := range valuesMap {
-			f := strings.Fields(k)
-			if len(f) < 2 {
-				continue
+			var n float64
+
+			if val != "" {
+				n, err = strconv.ParseFloat(val, 64)
+				if err != nil {
+					return nil, err
+				}
 			}
-			switch f[1] {
-			case "open":
-				quote.Open = val.(float64)
-			case "high":
-				quote.High = val.(float64)
-			case "low":
-				quote.Low = val.(float64)
-			case "close":
-				quote.Close = val.(float64)
-			case "volume":
-				quote.Volume = val.(float64)
+
+			switch {
+			case strings.HasSuffix(k, "open"):
+				quote.Open = n
+			case strings.HasSuffix(k, "high"):
+				quote.High = n
+			case strings.HasSuffix(k, "low"):
+				quote.Low = n
+			case strings.HasSuffix(k, "close"):
+				quote.Close = n
+			case strings.HasSuffix(k, "volume"):
+				quote.Volume = n
 			}
 		}
+
 		quotes = append(quotes, quote)
 	}
 
+	sort.Sort(QuotesInDecreasingDateOrder(quotes))
+
 	return quotes, nil
+}
+
+type QuotesInIncreasingDateOrder []Quote
+
+func (q QuotesInIncreasingDateOrder) Len() int {
+	return len(q)
+}
+
+func (q QuotesInIncreasingDateOrder) Less(i, j int) bool {
+	return q[i].Time.Before(q[j].Time)
+}
+
+func (q QuotesInIncreasingDateOrder) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+
+type QuotesInDecreasingDateOrder []Quote
+
+func (q QuotesInDecreasingDateOrder) Len() int {
+	return len(q)
+}
+
+func (q QuotesInDecreasingDateOrder) Less(i, j int) bool {
+	return q[j].Time.Before(q[i].Time)
+}
+
+func (q QuotesInDecreasingDateOrder) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
 }
--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package alphavantage_test
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -33,11 +34,46 @@ func TestService_Do(t *testing.T) {
 		t.Logf("got: %q", fakeClient.Recieves.Req.URL.Host)
 	}
 	if fakeClient.Recieves.Req.URL.Path != "/query" {
-		t.Error("it calls the correct host")
-		t.Logf("got: %q", fakeClient.Recieves.Req.URL.Host)
+		t.Error("it calls the endpoint")
+		t.Logf("got: %q", fakeClient.Recieves.Req.URL.Path)
 	}
 }
 
 func TestService_QueryStocks(t *testing.T) {
+	t.Run("when quote keys are daily", func(t *testing.T) {
+		buf, err := ioutil.ReadFile("test_data/query_time_series_weekly_ibm.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		quotes, err := alphavantage.ParseQuotesResponse(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(quotes) != 1043 {
+			t.Fail()
+		}
 
+		if v := quotes[len(quotes)-1].Volume; v != 22560100 {
+			t.Errorf("expected 250 got %f", v)
+		}
+
+		if v := quotes[0].Open; v != 104.4400 {
+			t.Errorf("expected 104.4400 got %f", v)
+		}
+	})
+
+	t.Run("when quote keys are intraday", func(t *testing.T) {
+		buf, err := ioutil.ReadFile("test_data/query_time_series_intraday_ibm.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		quotes, err := alphavantage.ParseQuotesResponse(buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if v := quotes[len(quotes)-1].Time.Hour(); v != 18 {
+			t.Errorf("expected 18 got %d", v)
+		}
+	})
 }
