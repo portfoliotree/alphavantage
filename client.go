fc5c1f3c2d325204c0cdb3e46a4b1d926570766f
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2021-09-21 01:54:44 -0700

    add column name to error messages

--- client.go
+++ client.go
@@ -191,13 +191,13 @@ func ParseCSV(r io.Reader, data interface{}, location *time.Location) error {
 			case reflect.Float64:
 				fl, err := strconv.ParseFloat(value, 64)
 				if err != nil {
-					return fmt.Errorf("failed to parse float64 value %q on row %d column %d: %w", value, rowIndex, columnIndex, err)
+					return fmt.Errorf("failed to parse float64 value %q on row %d column %d (%s): %w", value, rowIndex, columnIndex, header[columnIndex], err)
 				}
 				structValue.Elem().Field(fieldIndex).SetFloat(fl)
 			case reflect.Int:
 				in, err := strconv.ParseInt(value, 10, 64)
 				if err != nil {
-					return fmt.Errorf("failed to parse float64 value %q on row %d column %d: %w", value, rowIndex, columnIndex, err)
+					return fmt.Errorf("failed to parse float64 value %q on row %d column %d (%s): %w", value, rowIndex, columnIndex, header[columnIndex], err)
 				}
 				structValue.Elem().Field(fieldIndex).SetInt(in)
 			default:
@@ -215,7 +215,7 @@ func ParseCSV(r io.Reader, data interface{}, location *time.Location) error {
 				}
 				tm, err := time.ParseInLocation(layout, value, location)
 				if err != nil {
-					return fmt.Errorf("failed to parse time value %q on row %d column %d: %w", value, rowIndex, columnIndex, err)
+					return fmt.Errorf("failed to parse time value %q on row %d column %d (%s): %w", value, rowIndex, columnIndex, header[columnIndex], err)
 				}
 				structValue.Elem().Field(fieldIndex).Set(reflect.ValueOf(tm))
 			}
